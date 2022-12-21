648de338c8b2af356c5e86f0459f17bcd813c579
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-12-21 12:21:08 -0600

    Implemented StorageMessage component.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import './styles/colors.scss'
 import Management from './components/Management/Management'
 import StorageViewer from './components/StorageViewer/StorageViewer'
+import StorageMessage from './components/StorageMessage/StorageMessage'
 
 function App() {
   return (
     <div className="App">
       <Management />
       <StorageViewer />
+      <StorageMessage />
     </div>
   )
 }
