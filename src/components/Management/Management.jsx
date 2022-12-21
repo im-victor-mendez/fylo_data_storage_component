75120f1bb865a6ef201c68ce1c3508d4a537610d
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-12-21 14:08:17 -0600

    Animated Management component.

--- src/components/Management/Management.jsx
+++ src/components/Management/Management.jsx
@@ -5,18 +5,43 @@ import LOGO from "../../assets/icons/logo.svg";
 import DOCUMENT from "../../assets/icons/icon-document.svg";
 import FOLDER from "../../assets/icons/icon-folder.svg";
 import UPLOAD from "../../assets/icons/icon-upload.svg";
+import { motion } from 'framer-motion';
 
 function Management() {
   return (
-    <section id='management'>
-        <img src={LOGO} alt="" />
+    <motion.section id='management'
+    initial={{ opacity: 0, scale: 0, y: 200 }}
+    animate={{ opacity: 1, scale: 1, y: 0 }}
+    transition={{ duration: 0.5 }}
+    >
+        <motion.img src={LOGO} alt="Logo icon"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+        whileHover={{ scale: 1.2 }}
+        />
 
         <div id='management-files'>
-            <img src={DOCUMENT} alt="Document icon" />
-            <img src={FOLDER} alt="Folder icon" />
-            <img src={UPLOAD} alt="Upload icon" />
+            <motion.img src={DOCUMENT} alt="Document icon"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 1 }}
+            whileHover={{ scale: 1.2 }}
+            />
+            <motion.img src={FOLDER} alt="Folder icon"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 1 }}
+            whileHover={{ scale: 1.2 }}
+            />
+            <motion.img src={UPLOAD} alt="Upload icon"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 1 }}
+            whileHover={{ scale: 1.2 }}
+            />
         </div>
-    </section>
+    </motion.section>
   )
 }
 
