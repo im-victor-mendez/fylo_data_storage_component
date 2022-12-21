73fa4653d88c54d1a09bf883859a6f280b8ee162
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-12-21 12:20:40 -0600

    Component - Finished StorageMessage at the moment.

---
+++ src/components/StorageMessage/StorageMessage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './StorageMessage.scss'
+
+function StorageMessage() {
+  return (
+    <section id='storage-message'>
+        <b>185</b>
+        <p>GB LEFT</p>
+    </section>
+  )
+}
+
+export default StorageMessage
