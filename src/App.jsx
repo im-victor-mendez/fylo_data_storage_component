32284e384202f679a09bc6f66150644e81dbd04d
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-12-21 10:31:46 -0600

    Implemented constant colors and Management component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import './App.css'
+import './styles/colors.scss'
+import Management from './components/Management/Management'
 
 function App() {
   return (
     <div className="App">
+      <Management />
     </div>
   )
 }
