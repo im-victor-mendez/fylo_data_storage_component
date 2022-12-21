8ec588fabec2f758b7f3795579076b5e774ea8fe
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-12-21 12:38:20 -0600

    Styled for Mobile devices.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import './styles/colors.scss'
+import './styles/Mobile.scss'
 import Management from './components/Management/Management'
 import StorageViewer from './components/StorageViewer/StorageViewer'
 import StorageMessage from './components/StorageMessage/StorageMessage'
