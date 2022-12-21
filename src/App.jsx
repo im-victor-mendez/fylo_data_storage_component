2ac5a12172b0e5b534007ee34eedb7f9099e746a
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-12-21 11:21:34 -0600

    Implemented StorageViewer component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import './styles/colors.scss'
 import Management from './components/Management/Management'
+import StorageViewer from './components/StorageViewer/StorageViewer'
 
 function App() {
   return (
     <div className="App">
       <Management />
+      <StorageViewer />
     </div>
   )
 }
