9819b5b85d685fe47a913cec715e1e335cc941d2
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-12-21 14:21:15 -0600

    Animated StorageMessage component.

--- src/components/StorageMessage/StorageMessage.jsx
+++ src/components/StorageMessage/StorageMessage.jsx
@@ -1,12 +1,17 @@
+import { motion } from 'framer-motion'
 import React from 'react'
 import './StorageMessage.scss'
 
 function StorageMessage() {
   return (
-    <section id='storage-message'>
+    <motion.section id='storage-message'
+    initial={{ y: 100, opacity: 0, scale: 0 }}
+    animate={{ y: 0, opacity: 1, scale: 1 }}
+    transition={{ duration: 1 }}
+    >
         <h1>185</h1>
         <p>GB LEFT</p>
-    </section>
+    </motion.section>
   )
 }
 
